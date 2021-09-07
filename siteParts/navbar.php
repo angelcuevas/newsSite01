58e905ce7abf1d446a0da7dc462c6eadede62b09
christian <chriistiianleiiva@gmail.com>
2021-09-07 00:43:03 -0300

    Agregar titulos

--- siteParts/navbar.php
+++ siteParts/navbar.php
@@ -57,20 +57,19 @@
                                         <ul id="navigation">    
                                             <li><a href="index.html">Inicio</a></li>
                                             <?php 
-                                                //acá
+                                                foreach($categorias as $categoria){
+                                                    ?>                                                    
+                                                        <li> <a href=""><?php echo $categoria[nombre];?></a></li>                                                  
+                                                    <?php                                                   
+                                                }                                                
                                             ?>
-                                            <!-- <li><a href="categori.html">Category</a></li>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="latest_news.html">Latest News</a></li>
-                                            <li><a href="contact.html">Contact</a></li> -->
-                                            <!-- <li><a href="#">Pages</a>
-                                                <ul class="submenu">
-                                                    <li><a href="elements.html">Element</a></li>
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="single-blog.html">Blog Details</a></li>
-                                                    <li><a href="details.html">Categori Details</a></li>
-                                                </ul>
-                                            </li> -->
+                                            <!-- <ul class="submenu">
+                                                <li><a href="elements.html">Element</a></li>
+                                                <li><a href="blog.html">Blog</a></li>
+                                                <li><a href="single-blog.html">Blog Details</a></li>
+                                                <li><a href="details.html">Categori Details</a></li>
+                                            </ul> -->
+                                            </li>
                                         </ul>
                                     </nav>
                                 </div>
